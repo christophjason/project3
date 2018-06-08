a787e41a4942443a25441ca8e3eb3b4de245ef86
Christopher Jason <christopherjason98@gmail.com>
2018-06-08 23:35:10 +0800

    first commit

---
+++ NSHW/GUI.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace NSHW
+{
+    partial class GUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GUI));
+            this.save = new System.Windows.Forms.CheckBox();
+            this._udp = new System.Windows.Forms.CheckBox();
+            this._tcp = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.adapters_label = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.start_button = new System.Windows.Forms.Button();
+            this.adapters_list = new System.Windows.Forms.ComboBox();
+            this.stop_button = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // save
+            // 
+            this.save.AutoSize = true;
+            this.save.BackColor = System.Drawing.Color.Transparent;
+            this.save.Font = new System.Drawing.Font("Raleway", 11F);
+            this.save.ForeColor = System.Drawing.Color.Black;
+            this.save.Location = new System.Drawing.Point(317, 134);
+            this.save.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(138, 22);
+            this.save.TabIndex = 22;
+            this.save.Text = "Save Capturing";
+            this.save.UseVisualStyleBackColor = false;
+            // 
+            // _udp
+            // 
+            this._udp.AutoSize = true;
+            this._udp.BackColor = System.Drawing.Color.Transparent;
+            this._udp.Font = new System.Drawing.Font("Raleway", 12F);
+            this._udp.ForeColor = System.Drawing.Color.Black;
+            this._udp.Location = new System.Drawing.Point(502, 82);
+            this._udp.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this._udp.Name = "_udp";
+            this._udp.Size = new System.Drawing.Size(63, 23);
+            this._udp.TabIndex = 20;
+            this._udp.Text = "UDP";
+            this._udp.UseVisualStyleBackColor = false;
+            // 
+            // _tcp
+            // 
+            this._tcp.AutoSize = true;
+            this._tcp.BackColor = System.Drawing.Color.Transparent;
+            this._tcp.Font = new System.Drawing.Font("Raleway", 12F);
+            this._tcp.ForeColor = System.Drawing.Color.Black;
+            this._tcp.Location = new System.Drawing.Point(296, 84);
+            this._tcp.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this._tcp.Name = "_tcp";
+            this._tcp.Size = new System.Drawing.Size(59, 23);
+            this._tcp.TabIndex = 19;
+            this._tcp.Text = "TCP";
+            this._tcp.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Raleway", 12F);
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(52, 82);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 19);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Set filter :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Raleway", 8.25F);
+            this.textBox1.Location = new System.Drawing.Point(50, 452);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(660, 193);
+            this.textBox1.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Raleway", 11F);
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(47, 422);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 18);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Protocol Info :";
+            // 
+            // adapters_label
+            // 
+            this.adapters_label.AutoSize = true;
+            this.adapters_label.BackColor = System.Drawing.Color.Transparent;
+            this.adapters_label.Font = new System.Drawing.Font("Raleway", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.adapters_label.ForeColor = System.Drawing.Color.Black;
+            this.adapters_label.Location = new System.Drawing.Point(49, 43);
+            this.adapters_label.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.adapters_label.Name = "adapters_label";
+            this.adapters_label.Size = new System.Drawing.Size(198, 19);
+            this.adapters_label.TabIndex = 15;
+            this.adapters_label.Text = "Select Network Adapter :";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(50, 198);
+            this.listView1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(733, 204);
+            this.listView1.TabIndex = 14;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Time";
+            this.columnHeader2.Width = 182;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Source";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 142;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Destination";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader4.Width = 142;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Protocol";
+            this.columnHeader5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader5.Width = 150;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Length";
+            this.columnHeader6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader6.Width = 115;
+            // 
+            // start_button
+            // 
+            this.start_button.BackColor = System.Drawing.Color.Black;
+            this.start_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.start_button.Font = new System.Drawing.Font("Raleway", 12F);
+            this.start_button.ForeColor = System.Drawing.Color.White;
+            this.start_button.Location = new System.Drawing.Point(53, 124);
+            this.start_button.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(235, 38);
+            this.start_button.TabIndex = 13;
+            this.start_button.Text = "Start capturing";
+            this.start_button.UseVisualStyleBackColor = false;
+            this.start_button.Click += new System.EventHandler(this.start_button_Click);
+            // 
+            // adapters_list
+            // 
+            this.adapters_list.Font = new System.Drawing.Font("Raleway", 12F);
+            this.adapters_list.FormattingEnabled = true;
+            this.adapters_list.Location = new System.Drawing.Point(296, 40);
+            this.adapters_list.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.adapters_list.Name = "adapters_list";
+            this.adapters_list.Size = new System.Drawing.Size(453, 27);
+            this.adapters_list.TabIndex = 12;
+            // 
+            // stop_button
+            // 
+            this.stop_button.BackColor = System.Drawing.Color.Black;
+            this.stop_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.stop_button.Font = new System.Drawing.Font("Raleway", 12F);
+            this.stop_button.ForeColor = System.Drawing.Color.White;
+            this.stop_button.Location = new System.Drawing.Point(502, 124);
+            this.stop_button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.stop_button.Name = "stop_button";
+            this.stop_button.Size = new System.Drawing.Size(253, 38);
+            this.stop_button.TabIndex = 23;
+            this.stop_button.Text = "Stop capturing";
+            this.stop_button.UseVisualStyleBackColor = false;
+            this.stop_button.Click += new System.EventHandler(this.stop_button_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // GUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(832, 690);
+            this.Controls.Add(this.stop_button);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this._udp);
+            this.Controls.Add(this._tcp);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.adapters_label);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.start_button);
+            this.Controls.Add(this.adapters_list);
+            this.Font = new System.Drawing.Font("Raleway", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "GUI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sniffer Group 1";
+            this.Load += new System.EventHandler(this.GUI_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox save;
+        private System.Windows.Forms.CheckBox _udp;
+        private System.Windows.Forms.CheckBox _tcp;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label adapters_label;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.Button start_button;
+        private System.Windows.Forms.ComboBox adapters_list;
+        private System.Windows.Forms.Button stop_button;
+        private System.Windows.Forms.Timer timer1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+    }
+}
+
